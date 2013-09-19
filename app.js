6a8c6009fcc376bc62a7bc410908f4feca57629a
Mike Causer <mcauser@gmail.com>
2013-09-19 13:24:52 +1000

    More detailed server starting console message

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/login', routes.login);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port %d in mode %s", app.get('port'), app.get('env'));
   //process.setuid(config.uid);
   //process.setgid(config.gid);
 });
